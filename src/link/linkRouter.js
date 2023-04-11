51232bf361f4fc0fdb60f8bcfa0afe2919898483
rohit pandit <rohit0311@outlook.com>
2023-04-11 15:10:44 +0530

    incomplete work for the link

--- src/link/linkRouter.js
+++ src/link/linkRouter.js
@@ -1,1 +1,19 @@
-const router = ('./index')
+const express = require('express');
+const {verifyUser} = require('../user/userMiddleware');
+const router = express.Router();
+
+router.get('*', async (req, res)=>{
+    res.status(200);
+})
+
+router.post('/link', async (req, res)=>{
+    let {link} = req.body;
+    let result = {message:'', body: {}, status: 200};
+    if(!link){
+        result.status = 400;
+        result.message = 'Link is required!'
+        return res.status(result.status).json({body: result.body, message: result.message})
+    }
+
+    result = await 
+})
